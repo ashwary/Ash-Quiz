95a49b0f3979f516326426e833db23fb8e59cf9e
Aishwary Tandon <ashwarytandon@gmail.com>
2020-05-13 15:32:30 -0700

    Quiz Initial

---
+++ android/app/src/main/kotlin/com/example/ashquiz/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ashquiz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AshQuiz());
+}
+
+class AshQuiz extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.grey.shade900,
+    ));
+  }
+}
